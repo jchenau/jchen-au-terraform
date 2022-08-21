7fe4852fbc8e2c7fb75631c30457e85eec414cea
Junrui Chen <chenjunrui93@gmail.com>
2022-08-21 21:05:58 +1000

    Remove impersonate service account

--- src/google/projects/terraform/backend.tf
+++ src/google/projects/terraform/backend.tf
@@ -2,7 +2,5 @@ terraform {
   backend "gcs" {
     bucket = "jchen-au-terraform-state"
     prefix = "google/projects/terraform"
-
-    impersonate_service_account = "terraform@terraform-360104.iam.gserviceaccount.com"
   }
 }
--- src/google/projects/terraform/google.tf
+++ src/google/projects/terraform/google.tf
@@ -1,6 +1,4 @@
 provider "google" {
   project = var.project_id
   region  = var.location
-
-  impersonate_service_account = "terraform@terraform-360104.iam.gserviceaccount.com"
 }
